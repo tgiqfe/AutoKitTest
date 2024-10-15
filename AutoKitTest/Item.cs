360bcca944bebed699c96d63e2003d5ee8c1972c
tqji31001 <tqji31001@gmail.com>
2024-10-16 00:08:59 +0900

    First commit.

---
+++ AutoKitTest/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoKitTest
+{
+    internal class Item
+    {
+        public static readonly string WorkDir =
+            Path.Combine(Environment.ExpandEnvironmentVariables("%TEMP%"), "KitTest");
+    }
+}
---
+++ AutoKitTest/Lib/AutoGUI.cs
@@ -0,0 +1,49 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoKitTest.Lib
+{
+    internal class AutoGUI
+    {
+        private string _tempDir = Path.Combine(Item.WorkDir, "temp");
+        private string _tempScreenCapturePath = Path.Combine(Item.WorkDir, "temp", "screencapture.png");
+
+        public AutoGUI()
+        {
+            if (!Directory.Exists(_tempDir))
+            {
+                Directory.CreateDirectory(_tempDir);
+            }
+        }
+
+        public bool LocateOnScreen(string[] templateImagePaths)
+        {
+            
+            using (Bitmap screenCapture = ScreenCapture.FullScreen())
+            using (Mat screen = BitmapConverter.ToMat(screenCapture))
+            {
+                double threshold = 0.99;
+                foreach (var imagePath in templateImagePaths)
+                {
+                    using (Mat template = new Mat(imagePath, ImreadModes.Unchanged))
+                    using (Mat result = new Mat())
+                    {
+                        Cv2.MatchTemplate(screen, template, result, TemplateMatchModes.CCoeffNormed);
+                        OpenCvSharp.Point minLoc, maxLoc;
+                        double minVal, maxVal;
+                        Cv2.MinMaxLoc(result, out minVal, out maxVal, out minLoc, out maxLoc);
+
+                        return maxVal >= threshold;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
